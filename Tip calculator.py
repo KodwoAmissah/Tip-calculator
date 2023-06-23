fff8b28caed9f1162d2e514e36ff8dd51cc688b0
KodwoAmissah <137364174+KodwoAmissah@users.noreply.github.com>
2023-06-23 09:35:47 +0000

    Add files via upload

---
+++ Tip calculator.py
@@ -0,0 +1,8 @@
+Food_charge=float(input("Please enter the charge for food: $"))
+amount_on_tip=Food_charge * 18/100
+amount_on_sales_tax=Food_charge*7/100
+grand_total=Food_charge+amount_on_sales_tax+amount_on_tip
+print(f"${Food_charge:.2f}")
+print(f"${amount_on_tip:.2f}")
+print(f"$ slaes tax={amount_on_sales_tax,:2f}")
+print(f"$ grand total={grand_total:.2f}")
