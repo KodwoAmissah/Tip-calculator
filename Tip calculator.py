1af668181923574098cff4fce12d4527c7646675
Kodwo Amissah-Mensah <kodwoam@gmail.com>
2023-06-23 10:06:23 +0000

    Changing spelling mistakes in the code

--- Tip calculator.py
+++ Tip calculator.py
@@ -4,5 +4,5 @@ amount_on_sales_tax=Food_charge*7/100
 grand_total=Food_charge+amount_on_sales_tax+amount_on_tip
 print(f"${Food_charge:.2f}")
 print(f"${amount_on_tip:.2f}")
-print(f"$ slaes tax={amount_on_sales_tax:2f}")
+print(f"$ sales tax={amount_on_sales_tax:2f}")
 print(f"$ grand total={grand_total:.2f}")
